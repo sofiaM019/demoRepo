9aa57569571fb2b63c86925cd8c180af30f75d9f
Sofia Jameel <bsef23m019@pucit.edu.pk>
2024-10-15 00:26:20 +0500

    Update Array.h

--- Array.h
+++ Array.h
@@ -4,11 +4,8 @@ int * data;
 int capacity;
 bool isValidIndex(int index);
 piblic:
-Array(Array &&);
 Array(int cap=0,...);
 ~Array();
-Array & operator=(const Array & ref);
-void operator=(Array && ref);
 Array (const Array & );
 int & getsetcapacity();
 int getcapacity();
