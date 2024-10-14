4865a0941189dc797e7a4af19a711b5cff354cf0
Sofia Jameel <bsef23m019@pucit.edu.pk>
2024-10-15 00:03:17 +0500

    Create Array.h
    A header file which contains prototypes of user defined class named as Array .

---
+++ Array.h
@@ -0,0 +1,16 @@
+class Array 
+{ 
+int * data;
+int capacity;
+bool isValidIndex(int index);
+piblic:
+Array(Array &&);
+Array(int cap=0,...);
+~Array();
+Array & operator=(const Array & ref);
+void operator=(Array && ref);
+Array (const Array & );
+int & getsetcapacity();
+int getcapacity();
+void display()const;
+}
